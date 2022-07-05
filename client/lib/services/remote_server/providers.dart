f5b98815301939d50eded557536d0943d6cbafae
Maatteogekko <matteogent9991@gmail.com>
2022-07-05 22:41:41 +0200

    run on android

--- client/lib/services/remote_server/providers.dart
+++ client/lib/services/remote_server/providers.dart
@@ -1,12 +1,11 @@
-import 'dart:io';
-
 import 'package:client/proto/hello_world.pbgrpc.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:grpc/grpc_or_grpcweb.dart';
+import 'package:universal_platform/universal_platform.dart';
 
 final channelProvider = Provider<GrpcOrGrpcWebClientChannel>((ref) {
   return GrpcOrGrpcWebClientChannel.toSingleEndpoint(
-    host: Platform.isAndroid ? '10.0.2.2' : 'localhost',
+    host: UniversalPlatform.isAndroid ? '10.0.2.2' : 'localhost',
     port: 50051,
     transportSecure: false,
   );
